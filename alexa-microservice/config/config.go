7751a0dc083cc5c4ef4ed66ff62e9535971d2740
nmac-dev <ntwmac.dev@gmail.com>
2022-03-28 01:34:56 +0100

    Updated config w/ MS Cognitive Services, XML, SSML

--- alexa-microservice/config/config.go
+++ alexa-microservice/config/config.go
@@ -11,12 +11,35 @@ type Config struct {
 	
 	AppName string `json:"AppName"`
 
+	// WolframAlpha (Alpha)
 	WolframAlpha struct {
 		ApiURI		string `json:"apiURI"`
 		AppPath		string `json:"appPath"`
 		AppID		string `json:"appID"`
 	} `json:"WolframAlpha"`
 
+	// Microsoft Cognitive Services (MCS)
+	MicrosoftCS struct {
+		Region 		string `json:"region"`
+		TTS			string `json:"tts"`
+		STT			string `json:"stt"`
+		STTLang 	string `json:"sttLang"`
+		SrvPath 	string `json:"srvPath"`
+		AzureKey	string `json:"azureKey"`
+	} `json:"MicrosoftCS"`
+
+	// Extensible Markup Language (XML)
+	Xml struct {
+		Version 	string `xml:"version"`
+		Encoding	string `xml:"encoding"`
+		Lang 		string `xml:"lang"`
+	} `xml:"Xml"`
+	// Speech Synthesis Markup Language (SSML)
+	Ssml struct {
+		Voice struct {
+			Name 	string `xml:"name"`
+		} `xml:"voice"`
+	} `xml:"Ssml"`
 }
 
 // private package struct, ensures only one config exists
@@ -47,12 +70,24 @@ func getConfig() (*Config) {
 
 ////	Getters		////
 
-func GetAppName() 			string { return getConfig().AppName }
-func GetErrStatus()			error  { getConfig(); return cfgErr }
+func GetAppName()		string { return getConfig().AppName }
+func GetErrStatus()		error  { getConfig(); return cfgErr }
 
-func GetAlphaApiURI() 		string { return getConfig().WolframAlpha.ApiURI		}
-func GetAlphaAppPath()		string { return getConfig().WolframAlpha.AppPath	}
-func GetAlphaAppID() 		string { return getConfig().WolframAlpha.AppID		}
+func GetAlphaApiURI()	string { return getConfig().WolframAlpha.ApiURI	 }
+func GetAlphaAppPath()	string { return getConfig().WolframAlpha.AppPath }
+func GetAlphaAppID()	string { return getConfig().WolframAlpha.AppID	 }
+
+func GetMcsRegion()		string { return getConfig().MicrosoftCS.Region	 }
+func GetMcsTTS()		string { return getConfig().MicrosoftCS.TTS		 }
+func GetMcsSTT()		string { return getConfig().MicrosoftCS.STT		 }
+func GetMcsSTTLang()	string { return getConfig().MicrosoftCS.STTLang	 }
+func GetMcsSrvPath()	string { return getConfig().MicrosoftCS.SrvPath	 }
+func GetMcsAzureKey()	string { return getConfig().MicrosoftCS.AzureKey }
+
+func GetXmlVersion()	string { return getConfig().Xml.Version		}
+func GetXmlEncoding()	string { return getConfig().Xml.Encoding	}
+func GetXmlLang()		string { return getConfig().Xml.Lang		}
+func GetSsmlVoiceName()	string { return getConfig().Ssml.Voice.Name	}
 
 // Return the config in a JSON representation
 func ToString() string {
