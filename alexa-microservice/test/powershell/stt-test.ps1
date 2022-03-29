0dec5ec0de5562440fb72d644a0e5c310d4e2eb6
nmac-dev <ntwmac.dev@gmail.com>
2022-03-29 15:42:13 +0100

    Script cleanup

--- alexa-microservice/test/powershell/stt-test.ps1
+++ alexa-microservice/test/powershell/stt-test.ps1
@@ -4,8 +4,3 @@ $JSON='{"speech": "' + $WAVE + '"}'
 $JSON2 = Invoke-RestMethod -Method POST -Body $JSON -Uri @localhost:3002/stt | ConvertTo-Json -Compress
 
 Write-Output $JSON2
-
-
-# JSON="{\"speech\":\"‘base64 -i speech.wav‘\"}"
-# JSON2=‘curl -s -X POST -d "$JSON" localhost:3002/stt‘
-# echo $JSON2
